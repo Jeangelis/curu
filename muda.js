1a029f4807efca5d5ef5c3d223506c26a8a55ce7
Jeangelis <151084568+Jeangelis@users.noreply.github.com>
2023-12-14 19:59:52 -0300

    Add files via upload

---
+++ muda.js
@@ -0,0 +1,16 @@
+var muda = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -42.019916197085564,
+          -20.03235650668553
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
